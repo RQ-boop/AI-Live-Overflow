ea1849264d652773ad24094aa02094a73ff8cc31
RQ-boop <abc011029@qq.com>
2026-09-19 10:29:57 +0800

    Update app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.rqboop.deskpet"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.rqboop.deskpet"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "0.1"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    sourceSets["main"].assets.setSrcDirs(listOf("src/main/assets"))
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("org.json:json:20240303")
+}
